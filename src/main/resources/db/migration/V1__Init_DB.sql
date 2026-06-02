460d48f867267ede9a35d37047b553f6eac3b5f2
enemy <goldbyts@gmail.com>
2026-06-02 07:16:56 +0300

    feat: added migrations (flyway)

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,44 @@
+-- 1. Таблица зон доступа
+CREATE TABLE access_zone (
+    id BIGSERIAL PRIMARY KEY,
+    zone_name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- 2. Таблица клиентов (сотрудников)
+CREATE TABLE clients (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    is_active BOOLEAN NOT NULL
+);
+
+-- 3. Промежуточная таблица связей Many-to-Many (Права доступа)
+CREATE TABLE client_zone (
+    client_id BIGINT NOT NULL,
+    zone_id BIGINT NOT NULL,
+    PRIMARY KEY (client_id, zone_id),
+    CONSTRAINT fk_client_zone_client FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE,
+    CONSTRAINT fk_client_zone_zone FOREIGN KEY (zone_id) REFERENCES access_zone(id) ON DELETE CASCADE
+);
+
+-- 4. Таблица электронных карт One-to-One
+CREATE TABLE access_cards (
+    id BIGSERIAL PRIMARY KEY,
+    rfid_token VARCHAR(255) NOT NULL UNIQUE,
+    is_active BOOLEAN NOT NULL,
+    issued_at TIMESTAMP NOT NULL,
+    client_id BIGINT NOT NULL UNIQUE,
+    CONSTRAINT fk_access_cards_client FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
+);
+
+-- 5. Таблица исторических логов СКУД Many-to-One
+CREATE TABLE access_logs (
+    id BIGSERIAL PRIMARY KEY,
+    direction VARCHAR(255) NOT NULL,
+    time_stamp TIMESTAMP NOT NULL,
+    client_id BIGINT NOT NULL,
+    zone_id BIGINT NOT NULL,
+    CONSTRAINT fk_access_logs_client FOREIGN KEY (client_id) REFERENCES clients(id),
+    CONSTRAINT fk_access_logs_zone FOREIGN KEY (zone_id) REFERENCES access_zone(id)
+);
+
