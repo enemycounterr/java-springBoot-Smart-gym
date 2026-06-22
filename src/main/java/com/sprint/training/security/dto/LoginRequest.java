596c42e3e4612d8a93e85f28e978467e9c5b3af0
enemy <goldbyts@gmail.com>
2026-06-22 19:00:20 +0300

    feat: added Dockerfile for containerization an application

--- src/main/java/com/sprint/training/security/dto/LoginRequest.java
+++ src/main/java/com/sprint/training/security/dto/LoginRequest.java
@@ -3,7 +3,7 @@ package com.sprint.training.security.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @NotBlank(message = "Username cant be empty")
+        @NotBlank(message = "Username or Email cant be empty")
         String username,
 
         @NotBlank(message = "Pasword cant be empty")
