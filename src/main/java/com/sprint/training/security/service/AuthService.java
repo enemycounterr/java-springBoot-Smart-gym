6b8aca782087262d2c981746c25991d5490e984d
enemy <goldbyts@gmail.com>
2026-07-09 19:51:58 +0300

    refactor: removed unnecessary imports

--- src/main/java/com/sprint/training/security/service/AuthService.java
+++ src/main/java/com/sprint/training/security/service/AuthService.java
@@ -16,10 +16,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
