ecd2289f885c9ad494ff2b43f6f1ea6aacdb9a2f
enemycounter <enemycounter@gmail.com>
2026-07-23 21:29:13 +0300

    fix: move EnableCaching from TrainingApplication to RedisConfig

--- src/main/java/com/sprint/training/TrainingApplication.java
+++ src/main/java/com/sprint/training/TrainingApplication.java
@@ -2,10 +2,8 @@ package com.sprint.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class TrainingApplication {
 
 	public static void main(String[] args) {
